db7d037612188601937c4f723705c7b989583ba8
Mohammed O Rahman <Mohamme_Rahman@dell.com>
2018-10-31 19:07:54 +0530

    Implemented Remote actor invocation.

---
+++ AkkaDotNet/AkkaExample/AkkaExample.UserCoordinator.Remote/Program.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using Akka.Common;
+
+namespace AkkaExample.UserCoordinator.Remote
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+            ColorConsole.WriteLineGray("Creating MovieStreamingActorSystem in remote process");
+
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            MovieStreamingActorSystem.WhenTerminated.Wait();
+        }
+    }
+}

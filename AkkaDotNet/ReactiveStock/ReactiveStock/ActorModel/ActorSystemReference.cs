3dcd67734eef65ba20ea57f42f37ca903452d7e1
Mohammed O Rahman <Mohamme_Rahman@dell.com>
2018-11-13 12:11:52 +0530

    Initial WPF & Actor system setup

---
+++ AkkaDotNet/ReactiveStock/ReactiveStock/ActorModel/ActorSystemReference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.Ninject;
+using Ninject;
+
+namespace ReactiveStock.ActorModel
+{
+    public static class ActorSystemReference
+    {
+        public static ActorSystem ActorSystem { get; private set; }
+
+        static ActorSystemReference()
+        {
+            CreateActorSystem();
+        }
+
+        private static void CreateActorSystem()
+        {
+            ActorSystem = ActorSystem.Create("ReactiveStockActorSystem");
+
+            var container = new StandardKernel();
+
+            IDependencyResolver resolver = new NinjectDependencyResolver(container, ActorSystem);
+
+
+        }
+    }
+}

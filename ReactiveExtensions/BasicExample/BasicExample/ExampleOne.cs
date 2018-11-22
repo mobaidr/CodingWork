ae6e1b1ddc10776e1d61efacb5c402b9089d52b5
Mohammed O Rahman <Mohamme_Rahman@dell.com>
2018-11-22 13:19:25 +0530

    First Rx example

---
+++ ReactiveExtensions/BasicExample/BasicExample/ExampleOne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace BasicExample
+{
+    public static class ExampleOne
+    {
+        public static void BasicExample()
+        {
+            var query = from number in Enumerable.Range(1, 5) select number;
+
+            var oQuery = query.ToObservable();
+            oQuery.Subscribe(Console.WriteLine, ImDone);
+            Console.ReadKey();
+
+            void ImDone() { Console.WriteLine("I'm Done"); }
+        }
+    }
+}
---
+++ ReactiveExtensions/BasicExample/BasicExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleOne.BasicExample();
+        }
+    }
+}
